8c22b1d8493362eeae9f698a3708eb5b8b679d76
andrew albrecht <andrewalbrecht@utexas.edu>
2016-04-04 18:55:06 -0500

    {Towers of Hanoi}

--- apps/08towers-of-hanoi/script.js
+++ apps/08towers-of-hanoi/script.js
@@ -1,5 +1,30 @@
 'use strict';
 
-$(document).ready(function() {
-  // Put app logic here
-}
+$(document).ready(function () {
+    // Put app logic here
+    var block = null;
+
+    $('[data-stack]').click(function () {
+        if (block !== null) {
+            if (parseInt(block.attr('data-block')) < parseInt($(this).children().last().attr('data-block')) || $(this).children().length == 0) {
+                $(this).append(block);
+                block = null;
+                checkForWin();
+            }
+        } else {
+            block = $(this).children().last().detach();
+        }
+
+    });
+
+    function checkForWin() {
+        //$('[data-stack]').each(function () {
+        if ($('[data-stack="2"]').children().length == 4 || $('[data-stack="3"]').children().length == 4) {
+            $('#announce-game-won').text('You won!');
+        }
+
+    };
+
+});
+
+//separate detach
